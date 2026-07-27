0c3af3ccc910f762801ae979d00333870483a62f
Jacson R. Sasse <jacsonrsasse@gmail.com>
2026-07-27 00:16:55 -0300

    feat: create fetch products resource

--- lib/ecommerce-api-stack.ts
+++ lib/ecommerce-api-stack.ts
@@ -4,12 +4,24 @@ import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as cwlogs from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
 
+interface ECommerceApiStackProps extends cdk.StackProps {
+  productsFetchHandler: lambdaNodeJS.NodejsFunction;
+}
+
 export class ECommerceApiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
     super(scope, id, props);
 
     const api = new apigateway.RestApi(this, "ECommerceApi", {
       restApiName: "ecommerce-api",
     });
+
+    const productsFetchIntegration = new apigateway.LambdaIntegration(
+      props.productsFetchHandler,
+    );
+
+    // /products
+    const productsResource = api.root.addResource("products");
+    productsResource.addMethod("GET", productsFetchIntegration);
   }
 }
