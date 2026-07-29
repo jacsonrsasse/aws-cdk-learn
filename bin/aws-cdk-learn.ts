1ebc35c087aada4df80dd67b2291a6dd31189066
Jacson R. Sasse <jacsonrsasse@gmail.com>
2026-07-28 21:47:55 -0300

    chore: upgrade ministack version
    Fixed multi stack bug

--- bin/aws-cdk-learn.ts
+++ bin/aws-cdk-learn.ts
@@ -2,7 +2,6 @@
 import * as cdk from "aws-cdk-lib/core";
 import { ProductsAppStack } from "../lib/products-app-stack";
 import { ECommerceApiStack } from "../lib/ecommerce-api-stack";
-import { ECommerceSingleStack } from "../lib/ecommerce-single-stack";
 
 const app = new cdk.App();
 const env: cdk.Environment = {
@@ -13,27 +12,14 @@ const tags = {
   const: "ECommerce",
 };
 
-// Local deploys (MiniStack/LocalStack) can struggle to resolve
-// cross-stack references, so the multi-stack pattern is swapped for a
-// single-stack one there. Toggle with `-c singleStack=true`, e.g.:
-//   npx cdklocal deploy -c singleStack=true --all
-const useSingleStack = app.node.tryGetContext("singleStack") === "true";
+const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
+  tags,
+  env,
+});
 
-if (useSingleStack) {
-  new ECommerceSingleStack(app, "ECommerce", {
-    tags,
-    env,
-  });
-} else {
-  const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
-    tags,
-    env,
-  });
-
-  const eCommerceApiStack = new ECommerceApiStack(app, "ECommerceApi", {
-    productsFetchHandler: productsAppStack.productsFetchHandler,
-    tags,
-    env,
-  });
-  eCommerceApiStack.addStackDependency(productsAppStack);
-}
+const eCommerceApiStack = new ECommerceApiStack(app, "ECommerceApi", {
+  productsFetchHandler: productsAppStack.productsFetchHandler,
+  tags,
+  env,
+});
+eCommerceApiStack.addStackDependency(productsAppStack);
